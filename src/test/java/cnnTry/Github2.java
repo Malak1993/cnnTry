901db89a7d4b05d8b7169cde2bcb08ec005424f3
Malak1993 <127646362+Malak1993@users.noreply.github.com>
2023-05-01 17:06:26 -0500

    new class 2

---
+++ src/test/java/cnnTry/Github2.java
@@ -0,0 +1,12 @@
+package cnnTry;
+
+public class Github2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("malak");
+		System.out.println("hello");
+		System.out.println("yeey");
+	}
+
+}
