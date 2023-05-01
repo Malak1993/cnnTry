a7c073d06c23178ce2879312e5591bb11836c2f6
Malak1993 <127646362+Malak1993@users.noreply.github.com>
2023-05-01 17:15:07 -0500

    3rd class

---
+++ src/test/java/cnnTry/Github3.java
@@ -0,0 +1,18 @@
+package cnnTry;
+
+public class Github3 {
+
+	public static void main(String[] args) {
+		System.out.println("malak");
+		System.out.println("hello");
+		System.out.println("yeey");
+		System.out.println("malak");
+		System.out.println("hello");
+		System.out.println("yeey");
+		System.out.println("malak");
+		System.out.println("hello");
+		System.out.println("yeey");
+
+	}
+
+}
